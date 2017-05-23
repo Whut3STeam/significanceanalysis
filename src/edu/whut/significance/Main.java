76818f3ad2e699adcc4bd92a121f670436ee1ccb
SunMing <1229536801@qq.com>
2017-05-23 16:44:49 +0800

    first push

---
+++ src/edu/whut/significance/Main.java
@@ -0,0 +1,12 @@
+package edu.whut.significance;
+
+import edu.whut.significance.ui.MainForm;
+
+/**
+ * Created by SunMing on 2017/5/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainForm m=new MainForm();
+    }
+}
---
+++ src/edu/whut/significance/methods/saic/SaicPreprocess.java
@@ -0,0 +1,7 @@
+package edu.whut.significance.methods.saic;
+
+/**
+ * Created by SunMing on 2017/5/23.
+ */
+public class SaicPreprocess {
+}
